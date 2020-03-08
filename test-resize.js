61b9a60efeeab247ef8d53e338057520ca6b199c
freego555 <freego555@gmail.com>
2020-03-08 22:44:58 +0200

    Resized image saves as new file

--- test-resize.js
+++ test-resize.js
@@ -1,15 +1,31 @@
 'use strict'
 
-const Jimp = require('jimp')
+const jimp = require('jimp')
+const path = require('path')
 
-async function resizeImage(path, width, height = Jimp.AUTO) {
-    const image = await Jimp.read(path)
+async function resizeImage(imgPath, width, height) {
+    // Checking of input parameters
+    if (!imgPath) {
+        console.log('!!! Path to the image must be setted as the first parameter.')
+        return
+    }
+    if (Number.isNaN(width)) {
+        console.log('!!! Width for new image must be setted as the second parameter.')
+        return
+    }
+    height = (Number.isNaN(height)) ? jimp.AUTO : height
+
+    const image = await jimp.read(imgPath)
     await image.resize(width, height)
-    await image.writeAsync(path)
+    await image.writeAsync(path.dirname(imgPath)
+        + path.sep
+        + 'resized_'
+        + path.basename(imgPath))
     console.log('Resizing was succesful')
 }
 
 const imgPath = process.argv[2]
 const width = +process.argv[3]
 const height = +process.argv[4]
+
 resizeImage(imgPath, width, height)
