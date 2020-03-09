a33a70fb8d7b47e57e4f357b1d36e9d3cd332c85
freego555 <freego555@gmail.com>
2020-03-09 23:21:42 +0200

    Watermarking is added. Some refactoring.

--- test-resize.js
+++ test-resize.js
@@ -3,6 +3,16 @@
 const jimp = require('jimp')
 const path = require('path')
 
+async function main() {
+    const imgPath = process.argv[2]
+    const width = +process.argv[3]
+    const height = +process.argv[4]
+    const watermarkPath = process.argv[5]
+
+    let newImgPath = await resizeImage(imgPath, width, height)
+    await watermarkImageCopy(newImgPath, watermarkPath)
+}
+
 async function resizeImage(imgPath, width, height) {
     // Checking of input parameters
     if (!imgPath) {
@@ -14,18 +24,40 @@ async function resizeImage(imgPath, width, height) {
         return
     }
     height = (Number.isNaN(height)) ? jimp.AUTO : height
+    let newImgPath = path.dirname(imgPath)
+        + path.sep
+        + 'resized_'
+        + path.basename(imgPath)
 
     const image = await jimp.read(imgPath)
     await image.resize(width, height)
-    await image.writeAsync(path.dirname(imgPath)
-        + path.sep
-        + 'resized_'
-        + path.basename(imgPath))
+    await image.quality(80)
+    await image.writeAsync(newImgPath)
     console.log('Resizing was succesful')
+
+    return newImgPath
 }
 
-const imgPath = process.argv[2]
-const width = +process.argv[3]
-const height = +process.argv[4]
+async function watermarkImageCopy(destPath, watermarkPath) {
+    const newImgPath = path.dirname(destPath)
+        + path.sep
+        + 'watermarked_'
+        + path.basename(destPath)
 
-resizeImage(imgPath, width, height)
+    const destImage = await jimp.read(destPath)
+    const watermarkImage = await jimp.read(watermarkPath)
+
+    // calc the center of destination image
+    const x = destImage.bitmap.width / 2 - watermarkImage.bitmap.width / 2
+    const y = destImage.bitmap.height / 2 - watermarkImage.bitmap.height / 2
+
+    await destImage.composite(watermarkImage, x, y, {
+        opacitySource: 0.5
+    })
+    await destImage.writeAsync(newImgPath)
+    console.log('Watermarking was succesful')
+
+    return newImgPath
+}
+
+main()
