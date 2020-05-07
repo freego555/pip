892940ca7fef2aebd290854faf2ab363968f2916
Ihor Rohovets <freego555@gmail.com>
2020-05-07 22:56:00 +0300

    main.js, resize.js. Processing of images in the specified folder by worker-farm.

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 'use strict'
 
-const path = require('path')
+const config = require('config')
+const fs = require('fs')
 const workerFarm = require('worker-farm')
 const serviceResize = workerFarm(require.resolve('./resize'))
 
 async function main () {
-  //TODO: Read images from the directory in the loop and resize them by serviceResize
+  const srcFolderPath = config.has('srcFolderPath') ? config.get('srcFolderPath') : undefined
+
+  // Checking of input parameters
+  if (!srcFolderPath) {
+    console.error('!!! Source folder path must be setted in config.')
+    return
+  }
+
+  // Read images from the directory and resize them by serviceResize
+  await fs.readdir(srcFolderPath, (err, files) => {
+    for (let i = 0; i < files.length; i++) {
+      serviceResize(files[i], (err) => {
+        if (err) {
+          console.error(err)
+        }
+      })
+    }
+  })
 }
 
 main()
--- resize.js
+++ resize.js
@@ -4,60 +4,71 @@ const config = require('config')
 const jimp = require('jimp')
 const path = require('path')
 
-module.exports = async (imgPath, callback) => {
-  // Checking of input parameters
-  if (!imgPath) {
-    console.error('!!! Path to the image must be setted as the first parameter.')
-    return
-  }
-  if (config.has('destFolderPath')) {
-    const destFolderPath = config.get('destFolderPath')
-  } else {
-    console.error('!!! Destination folder for new image must be setted in config.')
-    return
-  }
-  if (config.has('newWidth')) {
-    const newWidth = config.get('newWidth')
-  } else {
-    console.error('!!! Width for new image must be setted in config.')
-    return
-  }
-  if (config.has('newHeight')) {
-    const newHeight = config.get('newHeight')
-  } else {
-    const newHeight = jimp.AUTO
-  }
-  if (config.has('watermarkImgPath')) {
-    const watermarkImgPath = config.get('watermarkImgPath')
-  } else {
-    console.error('!!! Path to watermark image must be setted in config.')
-    return
-  }
-  if (config.has('opacitySource')) {
-    const opacitySource = config.get('opacitySource')
-  } else {
-    console.error('!!! Opacity must be setted in config.')
-    return
-  }
+module.exports = async (imgName, callback) => {
+  try {
+    const srcFolderPath = config.has('srcFolderPath') ? config.get('srcFolderPath') : undefined
+    const destFolderPath = config.has('destFolderPath') ? config.get('destFolderPath') : undefined
+    const newWidth = config.has('newWidth') ? config.get('newWidth') : undefined
+    const newHeight = config.has('newHeight') ? config.get('newHeight') : jimp.AUTO
+    const watermarkImgPath = config.has('watermarkImgPath') ? config.get('watermarkImgPath') : undefined
+    const opacitySource = config.has('opacitySource') ? config.get('opacitySource') : undefined
 
-  const newImgPath = destFolderPath +
-    path.sep +
-    'resized_' +
-    path.basename(imgPath)
+    // Checking of input parameters
+    let isChecked = true
+    if (!imgName) {
+      console.error('!!! Image name must be setted as the first parameter.')
+      isChecked = false
+    }
+    if (!srcFolderPath) {
+      console.error('!!! Destination folder for new image must be setted in config.')
+      isChecked = false
+    }
+    if (!destFolderPath) {
+      console.error('!!! Destination folder for new image must be setted in config.')
+      isChecked = false
+    }
+    if (!newWidth) {
+      console.error('!!! Width for new image must be setted in config.')
+      isChecked = false
+    }
+    if (!watermarkImgPath) {
+      console.error('!!! Path to watermark image must be setted in config.')
+      isChecked = false
+    }
+    if (!opacitySource) {
+      console.error('!!! Opacity must be setted in config.')
+      isChecked = false
+    }
 
-  const destImage = await jimp.read(imgPath)
-  const watermarkImage = await jimp.read(watermarkImgPath)
+    if (isChecked) {
+      const imgPath = srcFolderPath +
+        path.sep +
+        imgName
+      const newImgPath = destFolderPath +
+        path.sep +
+        'resized_' +
+        path.basename(imgPath)
 
-  // calc the center of destination image
-  const x = destImage.bitmap.width / 2 - watermarkImage.bitmap.width / 2
-  const y = destImage.bitmap.height / 2 - watermarkImage.bitmap.height / 2
+      const srcImage = await jimp.read(imgPath)
+      const watermarkImage = await jimp.read(watermarkImgPath)
 
-  await destImage.resize(newWidth, newHeight)
-  await destImage.composite(watermarkImage, x, y, {
-    opacitySource: opacitySource
-  })
-  await destImage.quality(80)
-  await destImage.writeAsync(newImgPath)
+      await srcImage.resize(newWidth, newHeight)
 
-  callback(null)
+      // calc the center of source image
+      const x = srcImage.bitmap.width / 2 - watermarkImage.bitmap.width / 2
+      const y = srcImage.bitmap.height / 2 - watermarkImage.bitmap.height / 2
+
+      await srcImage.composite(watermarkImage, x, y, {
+        opacitySource: opacitySource
+      })
+      await srcImage.quality(80)
+      await srcImage.writeAsync(newImgPath)
+    } else {
+      throw 'There were some errors during resizing.'
+    }
+
+    callback(null)
+  } catch (e) {
+    callback(e)
+  }
 }
