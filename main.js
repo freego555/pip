7ba6a19381db3a6e5833803be36e4a8460a68788
Ihor Rohovets <freego555@gmail.com>
2020-05-10 09:27:11 +0300

    main.js. Module fs.notify is added. Resizing of new files in directory is added.

--- main.js
+++ main.js
@@ -2,20 +2,37 @@
 
 const config = require('config')
 const fs = require('fs')
+const notify = require('fs.notify')
 const workerFarm = require('worker-farm')
 const serviceResize = workerFarm(require.resolve('./resize'))
+const srcFolderPath = config.has('srcFolderPath') ? config.get('srcFolderPath') : undefined
 
 async function main () {
-  const srcFolderPath = config.has('srcFolderPath') ? config.get('srcFolderPath') : undefined
-
   // Checking of input parameters
-  if (!srcFolderPath) {
-    console.error('!!! Source folder path must be setted in config.')
-    return
-  }
+  if (srcFolderPath) {
+    resizeFilesFromDir(srcFolderPath)
 
+    // Add notifications about changes in source folder
+    const notifications = new notify(srcFolderPath)
+    notifications.on('change', (file, event) => {
+      if (event.localeCompare('rename') == 0) {
+        serviceResize(file, (err) => {
+          if (err) {
+            console.error(err)
+          }
+        })
+      }
+
+      // TODO: How to do nothing if it is removing of file
+    })
+  } else {
+    console.error('!!! Source folder path must be setted in config.')
+  }
+}
+
+async function resizeFilesFromDir (path) {
   // Read images from the directory and resize them by serviceResize
-  await fs.readdir(srcFolderPath, (err, files) => {
+  await fs.readdir(path, (err, files) => {
     for (let i = 0; i < files.length; i++) {
       serviceResize(files[i], (err) => {
         if (err) {
